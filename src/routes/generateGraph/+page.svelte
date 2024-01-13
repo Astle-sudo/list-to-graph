cc98451c83c5b217ab9e5205ad1019f86ef64712
Astle <67687557+Astle-sudo@users.noreply.github.com>
2024-01-13 12:24:41 +0530

    Update +page.svelte

--- src/routes/generateGraph/+page.svelte
+++ src/routes/generateGraph/+page.svelte
@@ -322,7 +322,7 @@
         border-radius: 10px;
         height: 60vh;
         width: 15vw;
-        margin-top: 15vh;
+        margin-top: 20vh;
         overflow-y: scroll;
         box-shadow: 1px 1px 1px 1px white;
         color: white;
