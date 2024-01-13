5bc55eef5cc9cac103f3fe2e1211314270e73984
Astle <67687557+Astle-sudo@users.noreply.github.com>
2024-01-13 12:19:28 +0530

    Update +page.svelte

--- src/routes/generateGraph/+page.svelte
+++ src/routes/generateGraph/+page.svelte
@@ -199,7 +199,7 @@
         }
 
         p5.setup = () => {
-            p5.createCanvas(1145, 930);
+            p5.createCanvas(1145, 900);
             
             stateSpace = new Graph(false);
             
